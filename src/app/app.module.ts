cefb432dcca1e011922cfd90535902863d8fde40
manuc352 <emmanuel.ch20@gmail.com>
2020-01-23 15:29:58 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { Q1Component } from './q1/q1.component';
+import { FormsModule } from '@angular/forms';
+import { FilterPokemonPipePipe } from './filter-pokemon--pipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent,
+    Q1Component,
+    FilterPokemonPipePipe
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
